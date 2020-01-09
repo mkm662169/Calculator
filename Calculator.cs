b0d8eede05c82771545e6b10573a54f366826e87
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-09 10:21:54 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Operations;
+
+namespace MainCalc
+{
+    public class Calculator
+    {
+        public int solution;
+        public int Add(int a, int b)
+        {
+            solution = Addition.Add(a, b);
+            return solution;
+        }
+        public int Minus(int a, int b)
+        {
+            solution = Subtraction.Minus(a, b);
+            return solution;
+        }
+        public int Square(int a)
+        {
+            solution = Squared.Square(a);
+            return solution;
+        }
+        public int Multiply(int a, int b)
+        {
+            solution = a * b;
+            return solution;
+        }
+        public int Cube(int a)
+        {
+            solution = a * a * a;
+            return solution;
+        }
+    }
+}
